b4367036aafc2fdb44f5ea90468273eb9b823e57
BlackHat(س.مرتضی) <www.mortezamosavy0910@gmail.com>
2016-02-10 18:02:00 +0330

    Create Version.lua

---
+++ plugins/Version.lua
@@ -0,0 +1,25 @@
+do
+
+function run(msg, matches)
+  return 'Umbrella Telegram Bot v1.5'.. [[ 
+  
+  Website:
+  
+  Antispam Bot: @monsterTG
+  Channel: @blackhatchannel
+  Sudo (Admin): @black_hat_admin02
+  (بات انتی اسپمر هیولا تلگرام)
+  
+  ]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^[!/]ver$"
+  }, 
+  run = run 
+}
+
+end
